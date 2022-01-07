687fffe0a269a1c56e73ff74e541652b59045145
Eric Faris <ericfaris@gmail.com>
2022-01-06 23:11:57 -0500

    added additional pipelines to help with aggregating scores

--- src/helpers/pipelineHelper.js
+++ src/helpers/pipelineHelper.js
@@ -1,31 +1,8 @@
 const { ObjectId } = require("mongodb");
 
-class SearchPipelineHelper {
-    constructor(searchTerm) {
-        this.pipeline = [
-            { $match: {tableName:{$regex:'.*' + searchTerm + '.*', $options: 'i'}}},
-            { $unwind: "$authors" },
-            { $unwind: { "path": "$authors.versions", "preserveNullAndEmptyArrays": true } },
-            { $project: {
-              tableId: { $toString: "$_id" },
-              tableName: '$tableName',
-              authorId: { $toString: "$authors._id" },
-              authorName: "$authors.authorName",
-              versionId: { $toString: "$authors.versions._id" },
-              versionNumber: '$authors.versions.versionNumber',
-              tableUrl: '$authors.versions.versionUrl',
-              scores: '$authors.versions.scores',
-              postUrl: { $last: '$authors.versions.scores.postUrl'},
-              _id: 0
-            }},
-            { $sort: { tableName: 1, authorName: -1, versionNumber: -1 } }
-          ];
-    }
-}
-
-class SearchScorePipelineHelper {
-  constructor(id) {
-    this.pipeline = [
+class ScorePipelineHelper {
+  constructor(tableName, authorName, versionNumber) {
+    this.pipelineScoresByTable = [
       { $unwind: "$authors" },
       { $unwind: { "path": "$authors.versions", "preserveNullAndEmptyArrays": true } },
       { $unwind: { "path": "$authors.versions.scores", "preserveNullAndEmptyArrays": true } },
@@ -36,7 +13,6 @@ class SearchScorePipelineHelper {
         authorName: "$authors.authorName",
         versionId: '$authors.versions._id',
         versionNumber: '$authors.versions.versionNumber',
-        tableUrl: '$authors.versions.versionUrl',
         scoreId: '$authors.versions.scores._id',
         user: '$authors.versions.scores.user',
         userName: '$authors.versions.scores.username',
@@ -45,60 +21,148 @@ class SearchScorePipelineHelper {
         postUrl: '$authors.versions.scores.postUrl',
         _id: 0
       }},
-      { $sort: {tableName: 1, authorName: -1, versionNumber: -1, score: -1} },
+      { $sort: {tableName: 1, score: -1} },
       { $group: {
         _id: {
-          tableId: { $toString: "$_id" },
-          tableName: '$tableName',
-          authorId: { $toString: "$authors._id" },
-          authorName: "$authors.authorName",
-          versionId: { $toString: "$authors.versions._id" },
-          versionNumber: '$authors.versions.versionNumber',
-          tableUrl: '$authors.versions.versionUrl',
-          scoreId: { $toString: "$authors.versions.scores._id" },
-          user: '$authors.versions.scores.user',
-          userName: '$authors.versions.scores.username',
-          score: '$authors.versions.scores.score',
-          posted: '$authors.versions.scores.createdAt',
-          postUrl: '$authors.versions.scores.postUrl',
-          },
-        group: {$first:'$$ROOT'}
-      }},
-      { $replaceRoot:{newRoot:"$group"} },
-      { $sort: {tableName: 1, authorName: -1, versionNumber: -1} },
-      { $match: {
-        "$expr": {
-          "$or": [
-            { "$eq": ["$scoreId", ObjectId(id)] },
-            { "$eq": ["$versionId", ObjectId(id)] },
-            { "$eq": ["$authorId", ObjectId(id)] }
-          ]
+          tableId: "$tableId",
+          tableName: "$tableName",
         },
-      }}
+        scores: {
+          $push: {
+            authorId: '$authorId',
+            authorName: '$authorName',
+            versionId: '$versionId',
+            versionNumber: '$versionNumber',
+            scoreId: '$scoreId',
+            user: '$user',
+            userName: '$username',
+            score: '$score',
+            posted: '$posted',
+            postUrl: '$postUrl'
+          }
+        }
+      }},
+      { $sort: {tableName: 1} },
+      { $project: {
+        tableId: '$_id.tableId',
+        tableName: '$_id.tableName',
+        scores: "$scores",
+        _id: 0
+      }},
     ];
+
+    if (tableName) { this.pipelineScoresByTable.splice(4, 0, { $match: {'tableName': tableName}})};
+
+    this.pipelineScoresByTableAndAuthor = [
+      { $unwind: "$authors" },
+      { $unwind: { "path": "$authors.versions", "preserveNullAndEmptyArrays": true } },
+      { $unwind: { "path": "$authors.versions.scores", "preserveNullAndEmptyArrays": true } },
+      { $project: {
+        tableId: '$_id',
+        tableName: '$tableName',
+        authorId: '$authors._id',
+        authorName: "$authors.authorName",
+        versionId: '$authors.versions._id',
+        versionNumber: '$authors.versions.versionNumber',
+        scoreId: '$authors.versions.scores._id',
+        user: '$authors.versions.scores.user',
+        userName: '$authors.versions.scores.username',
+        score: '$authors.versions.scores.score',
+        posted: '$authors.versions.scores.createdAt',
+        postUrl: '$authors.versions.scores.postUrl',
+        _id: 0
+      }},
+      { $sort: {tableName: 1, authorName: 1, score: -1} },
+      { $group: {
+        _id: {
+          tableId: "$tableId",
+          tableName: "$tableName",
+          authorId: '$authorId',
+          authorName: "$authorName",  
+        },
+        scores: {
+          $push: {
+            versionId: '$versionId',
+            versionNumber: '$versionNumber',
+            scoreId: '$scoreId',
+            user: '$user',
+            userName: '$username',
+            score: '$score',
+            posted: '$posted',
+            postUrl: '$postUrl'
+          }
+        }
+      }},
+      { $sort: {tableName: 1, authorName: 1} },
+      { $project: {
+        tableId: '$_id.tableId',
+        tableName: '$_id.tableName',
+        authorId: '$_id.authorId',
+        authorName: '$_id.authorName',
+        scores: "$scores",
+        _id: 0
+      }},
+    ];
+
+    if (tableName && authorName) { this.pipelineScoresByTableAndAuthor
+      .splice(4, 0, { $match: {'tableName': tableName, 'authorName': authorName}})};
+
+    this.pipelineScoresByTableAndAuthorAndVersion = [
+      { $unwind: "$authors" },
+      { $unwind: { "path": "$authors.versions", "preserveNullAndEmptyArrays": true } },
+      { $unwind: { "path": "$authors.versions.scores", "preserveNullAndEmptyArrays": true } },
+      { $project: {
+        tableId: '$_id',
+        tableName: '$tableName',
+        authorId: '$authors._id',
+        authorName: "$authors.authorName",
+        versionId: '$authors.versions._id',
+        versionNumber: '$authors.versions.versionNumber',
+        scoreId: '$authors.versions.scores._id',
+        user: '$authors.versions.scores.user',
+        userName: '$authors.versions.scores.username',
+        score: '$authors.versions.scores.score',
+        posted: '$authors.versions.scores.createdAt',
+        postUrl: '$authors.versions.scores.postUrl',
+        _id: 0
+      }},
+      { $sort: {tableName: 1, authorName: 1, score: -1} },
+      { $group: {
+        _id: {
+          tableId: "$tableId",
+          tableName: "$tableName",
+          authorId: '$authorId',
+          authorName: "$authorName",
+          versionId: '$versionId',
+          versionNumber: '$versionNumber',  
+        },
+        scores: {
+          $push: {
+            scoreId: '$scoreId',
+            user: '$user',
+            userName: '$username',
+            score: '$score',
+            posted: '$posted',
+            postUrl: '$postUrl'
+          }
+        }
+      }},
+      { $sort: {tableName: 1, authorName: 1} },
+      { $project: {
+        tableId: '$_id.tableId',
+        tableName: '$_id.tableName',
+        authorId: '$_id.authorId',
+        authorName: '$_id.authorName',
+        versionId: '$_id.versionId',
+        versionNumber: '$_id.versionNumber',  
+        scores: "$scores",
+        _id: 0
+      }},
+    ];
+
+    if (tableName && authorName && versionNumber) { this.pipelineScoresByTableAndAuthorAndVersion
+      .splice(4, 0, { $match: {'tableName': tableName, 'authorName': authorName, 'versionNumber': versionNumber}})};
   }
 }
 
-class AllPipelineHelper {
-  constructor() {
-      this.pipeline = [
-          { $unwind: "$authors" },
-          { $unwind: { "path": "$authors.versions", "preserveNullAndEmptyArrays": true } },
-          { $project: {
-            tableId: { $toString: "$_id" },
-            tableName: '$tableName',
-            authorId: { $toString: "$authors._id" },
-            authorName: "$authors.authorName",
-            versionId: { $toString: "$authors.versions._id" },
-            versionNumber: '$authors.versions.versionNumber',
-            tableUrl: '$authors.versions.versionUrl',
-            scores: '$authors.versions.scores',
-            postUrl: { $last: '$authors.versions.scores.postUrl'},
-            _id: 0
-          }},
-          { $sort: { tableName: 1, authorName: -1, versionNumber: -1 } }
-      ];
-  }
-}
-
-module.exports = { SearchPipelineHelper, SearchScorePipelineHelper, AllPipelineHelper  }
+module.exports = { ScorePipelineHelper }
--- src/routers/api.v1.js
+++ src/routers/api.v1.js
@@ -1,26 +1,65 @@
 const mongoHelper = require('../helpers/mongoHelper');
-var express = require('express');
-var router = express.Router();
+const { ScorePipelineHelper } = require('../helpers/pipelineHelper');
+const express = require('express');
+const textToImage = require('text-to-image');
+const ImageDataURI = require('image-data-uri');
+const router = express.Router();
 
 router.get('/', function (req, res) {
     res.send('API Home Page');
 })
+
+router.post('/convert', async (req, res) => {
+    let textToConvert = req.body.text;
+    let filePath = req.body.filePath;
+    let imageOptions = {
+        maxWidth: 720,
+        fontSize: 76,
+        fontFamily: 'Arial',
+        lineHeight: 80,
+        margin: 20,
+        bgColor: 'black',
+        textColor: 'yellow',
+    };
+
+    const dataUri = await textToImage.generate(textToConvert, imageOptions);
+    ImageDataURI.outputFile(dataUri, filePath);
+    // res.sendStatus(200);
+    res.send(dataUri);
+});
 
 router.get('/tables', async (req, res) => {
     const tables = await mongoHelper.getAll('tables');
     res.send(tables);
 });
 
-// get table scores
-router.get('/scores', async (req, res) => {
+router.get('/scoresByTable', async (req, res) => {
     let tableName = req.query.tableName;
-    let authorName = req.query.authorName;
-    let version = req.query.version;
-    let pipeline = (new SearchPipelineHelper(tableName, authorName, version)).pipeline;
-
-    const tables = await mongoHelper.aggregate(pipeline, 'tables');
-    res.send(tables);
+    let pipeline = (new ScorePipelineHelper(tableName, null, null)).pipelineScoresByTable;
+    const table = await mongoHelper.aggregate(pipeline, 'tables');
+    res.send(table);
 });
 
+router.get('/scoresByTableAndAuthor', async (req, res) => {
+    let tableName = req.query.tableName;
+    let authorName = req.query.authorName;
+    let pipeline = (new ScorePipelineHelper(tableName, authorName, null)).pipelineScoresByTableAndAuthor;
+    const table = await mongoHelper.aggregate(pipeline, 'tables');
+    res.send(table);
+});
+
+router.get('/scoresByTableAndAuthorAndVersion', async (req, res) => {
+    let tableName = req.query.tableName;
+    let authorName = req.query.authorName;
+    let versionNumber = req.query.versionNumber;
+    let pipeline = (new ScorePipelineHelper(tableName, authorName, versionNumber)).pipelineScoresByTableAndAuthorAndVersion;
+    const table = await mongoHelper.aggregate(pipeline, 'tables');
+    res.send(table);
+});
+
+router.get('/weeks', async (req, res) => {
+    const weeks = await mongoHelper.getAll('weeks');
+    res.send(weeks);
+});
 
 module.exports = router;
